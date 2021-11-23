f1b9208858ff7bfe46cb6b96d981e4b0afc61788
Gleicon Moraes <gleicon@gmail.com>
2021-11-23 13:23:21 -0300

    connect write handler w/ lineprotocol

--- src/handlers.rs
+++ src/handlers.rs
@@ -30,16 +30,31 @@ async fn query_timeseries(
         Err(e) => {
             return Ok(HttpResponse::BadRequest()
                 .content_type("application/json")
-                .body(format!("Query timeseries")));
+                .body(format!("Query timeseries error: {}", e)));
         }
     }
 }
 
+/*
+ * curl -i -XPOST 'http://localhost:8086/api/v2/write?bucket=db/rp&precision=ns' \
+  --header 'Authorization: Token username:password' \
+  --data-raw 'cpu_load_short,host=server01,region=us-west value=0.64 1434055562000000000'
+*/
 #[post("/write")]
 async fn write_timeseries(
-    pm: web::Data<crate::persistence::TimeseriesDiskPersistenceManager>,
+    req_body: String,
+    pm: web::Data<Mutex<crate::persistence::TimeseriesDiskPersistenceManager>>,
 ) -> Result<HttpResponse, Error> {
-    return Ok(HttpResponse::BadRequest()
-        .content_type("application/json")
-        .body(format!("Write timeseries")));
+    match crate::protocol::LineProtocol::parse(req_body.clone()) {
+        Ok(b) => {
+            return Ok(HttpResponse::Ok()
+                .content_type("application/json")
+                .json(format!("{:?}", b)));
+        }
+        Err(e) => {
+            return Ok(HttpResponse::BadRequest()
+                .content_type("application/json")
+                .body(format!("{}", e)));
+        }
+    }
 }
